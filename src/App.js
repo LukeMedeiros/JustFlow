d5d7d0c1ad4b5e23b275f94e4964a80bac83587a
Luke Medeiros <imlukemedeiros@gmail.com>
2020-02-11 17:09:23 -0500

    changing the server ip

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
 
 		let parsed = queryString.parse(window.location.search)
 		if(parsed['access_token']===undefined){
-			window.location = 'http://localhost:8080/';
+			window.location = 'http://10.0.0.24:8080/';
 			this.setState({authenticated: true})
 		}
 		else if(this.state.firstAuth !== false){
